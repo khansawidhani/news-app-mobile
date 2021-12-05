352dc86d830d8939f45f187491a9eb0feaf7c022
khansawidhani <khansa.ashraf23@gmail.com>
2021-12-05 23:34:24 +0500

    Added new Get Api requests

--- lib/app/data/repository/news_repo.dart
+++ lib/app/data/repository/news_repo.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:news_app_flutter/app/data/model/article.dart';
+import 'package:news_app_flutter/app/data/model/news_data.dart';
 import 'package:news_app_flutter/app/data/services/http_service.dart';
 
 class NewsRepository{
@@ -10,7 +11,16 @@ class NewsRepository{
   }
   Future<List<Article>> getTopHeadlines() async{
     try{
-      final response = await httpService!.getRequest('/v2/top-headlines?country=us&category=business');
+      final response = await httpService!.getRequest('/v2/top-headlines?country=us');
+      final parsedResponse = NewsData.fromJson(response.data);
+      return parsedResponse.articles;
+    }on Exception catch(error){
+      throw Exception(error);
+    }
+  }
+  Future<List<Article>> getNewsByCategory(String category) async{
+    try{
+      final response = await httpService!.getRequest('/v2/top-headlines?language=en&category=$category');
       final parsedResponse = NewsData.fromJson(response.data);
       return parsedResponse.articles;
     }on Exception catch(error){
