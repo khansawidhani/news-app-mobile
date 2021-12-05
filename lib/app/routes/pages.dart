284b26cb09080172ea74938c05ed38e49458c40f
khansawidhani <khansa.ashraf23@gmail.com>
2021-12-05 23:30:18 +0500

    Modified routes

--- lib/app/routes/pages.dart
+++ lib/app/routes/pages.dart
@@ -1,23 +1,23 @@
 import 'package:get/route_manager.dart';
-import 'package:news_app_flutter/app/modules/dashboard/binding.dart';
+import 'package:news_app_flutter/app/modules/bookmarks/view.dart';
 import 'package:news_app_flutter/app/modules/dashboard/view.dart';
-import 'package:news_app_flutter/app/modules/favourites/binding.dart';
-import 'package:news_app_flutter/app/modules/favourites/view.dart';
-import 'package:news_app_flutter/app/modules/home_page/binding.dart';
-import 'package:news_app_flutter/app/modules/home_page/view.dart';
+import 'package:news_app_flutter/app/modules/home/view.dart';
+import 'package:news_app_flutter/app/modules/news/view.dart';
 import 'package:news_app_flutter/app/modules/user_auth/login/view.dart';
 import 'package:news_app_flutter/app/modules/user_auth/signup/view.dart';
 import 'package:news_app_flutter/app/routes/routes.dart';
 
 class AddPages{
-  static final pages = [
-    GetPage(name: Routes.home, page: ()=> HomePage(), binding: HomeBinding()),
-    GetPage(name: Routes.login, page: () => LoginScreen()),
-    GetPage(name: Routes.signup, page: () => SignupScreen()),
-    GetPage(name: Routes.favourites, page: () => Favourites(), binding: BookmarksBinding()),
-    GetPage(name: Routes.dashboard, page: () => Dashboard(), binding: DashboardBinding()),
 
-    
+  static final pages = [
+
+    GetPage(name: Routes.home, page: ()=> HomeScreen()),
+    GetPage(name: Routes.login, page: () => LoginScreen(),),
+    GetPage(name: Routes.signup, page: () => SignupScreen()),
+    GetPage(name: Routes.favourites, page: () => BookmarksScreen(), ),
+    GetPage(name: Routes.dashboard, page: () => Dashboard(),),
+    GetPage(name: Routes.news, page: () => NewsScreen()),
 
   ];
+
 }
--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,9 +1,12 @@
 class Routes{
+
   static const home = '/home';
   static const login = '/login';
   static const signup = '/signup';
   static const search = '/search';
   static const favourites = '/favourites';
   static const dashboard = '/dashboard';
+  static const news = '/news';
+  static const welcome = '/welcome';
    
 }
